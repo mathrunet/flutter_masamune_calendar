31c6397667f48fe4562f082f926fbc5ed93522a2
mathru <user@mathru.net>
2020-07-28 17:59:32 +0900

    Initial commit.

---
+++ example/main.dart
@@ -0,0 +1,3 @@
+import 'package:masamune_calendar/masamune_calendar.dart';
+
+void main() async {}
---
+++ lib/masamune_calendar.dart
@@ -0,0 +1,11 @@
+// Copyright 2020 mathru. All rights reserved.
+
+/// Masamune calendar framework library.
+///
+/// To use, import `package:masamune_calendar/masamune_calendar.dart`.
+///
+/// [mathru.net]: https://mathru.net
+/// [YouTube]: https://www.youtube.com/c/mathrunetchannel
+library masamune.calendar;
+
+export 'package:masamune_flutter/masamune_flutter.dart';
